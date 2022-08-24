c462aa7beb974cf3f910de89ddd97535e421a9ca
Shruthi Kamath <shruthi.dkamath6@gmail.com>
2022-08-24 18:46:46 +0530

    add : creating server v2

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const http = require("http");
 
-function handelRequest (request, response) {
+function handelRequest(request, response) {
+  if (request.url === "/currenttime") {
+    //localhost:3000/current time
     response.statusCode = 200;
-    response.end('<h1>Hello World!<h2>')
+    response.end("<h1>" + new Date().toISOString() + "<\h1>");
+  } else if (request.url === "/") {
+    //localhost:3000
+    response.statusCode = 200;
+    response.end("<h1>Hello World!<\h1>");
+  }
 }
+
 const server = http.createServer(handelRequest);
 
 server.listen(3000);
-
-
