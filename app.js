c30f94f4b09a27747a14bb1f9c2b0d65b140061f
Shruthi Kamath <shruthi.dkamath6@gmail.com>
2022-08-25 13:09:41 +0530

    add : init code

---
+++ app.js
@@ -0,0 +1,14 @@
+//With Express
+const express = require("express");
+
+const app = express();
+
+app.get("/currenttime", function(req, res) {
+    res.send("<h1>" + new Date().toISOString() + "<\h1>");
+}); //localhost:3000/current time
+
+app.get("/", function(req,res) {
+    res.send("<h1>Hello World!<\h1>"); //localhost:3000
+})
+app.listen (3000);
+
